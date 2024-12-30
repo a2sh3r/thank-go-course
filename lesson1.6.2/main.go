4043fd3ab0c7db65a08a4a7cb5934ad792556c8b
Michael Zainutdinov <michaelzainutdinov@ya.ru>
2024-12-31 01:42:38 +0300

    1.6.2

---
+++ lesson1.6.2/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// element - интерфейс элемента последовательности
+type element interface{}
+
+// weightFunc - функция, которая возвращает вес элемента
+type weightFunc func(element) int
+
+// iterator - интерфейс, который умеет
+// поэлементно перебирать последовательность
+type iterator interface {
+	next() bool
+	val() int
+}
+
+// intIterator - итератор по целым числам
+// (реализует интерфейс iterator)
+type intIterator struct {
+	nums []int
+}
+
+func (i intIterator) next() bool {
+	return len(i.nums) > 0
+}
+
+func (i *intIterator) val() int {
+	curr := i.nums[0]
+	i.nums = i.nums[1:]
+	return curr
+}
+
+// конструктор intIterator
+func newIntIterator(src []int) *intIterator {
+	return &intIterator{src}
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// main находит максимальное число из переданных на вход программы.
+func main() {
+	nums := readInput()
+	it := newIntIterator(nums)
+	weight := func(el element) int {
+		return el.(int)
+	}
+	m := max(it, weight)
+	fmt.Println(m)
+}
+
+// max возвращает максимальный элемент в последовательности.
+// Для сравнения элементов используется вес, который возвращает
+// функция weight.
+func max(it iterator, weight weightFunc) element {
+	var maxEl element
+	for it.next() {
+		curr := it.val()
+		if maxEl == nil || weight(curr) > weight(maxEl) {
+			maxEl = curr
+		}
+	}
+	return maxEl
+}
+
+// readInput считывает последовательность целых чисел из os.Stdin.
+func readInput() []int {
+	var nums []int
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		nums = append(nums, num)
+	}
+	return nums
+}
